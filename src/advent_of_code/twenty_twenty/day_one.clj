e4e109a9de65abb9f37e192e1b81e54581eee292
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2020-12-02 03:15:09 +1100

    1st day done ✅

---
+++ src/advent_of_code/twenty_twenty/day_one.clj
@@ -0,0 +1,53 @@
+(ns advent-of-code.twenty-twenty.day-one
+  (:require [clojure.string :refer [split]]))
+
+; Ref: https://adventofcode.com/2020/day/1
+
+(def input
+  (let [file (slurp "resources/day_one_input.txt")
+        parsed (split file #"\n")]
+    (map (fn [x] (Integer/parseInt x)) parsed)))
+
+(defn find-match-pair
+  "Find the matching pair that sums up to the amount"
+  ([xs] (find-match-pair xs 2020))
+  ([xs amount]
+   (if-let [ttl (count xs)]
+     (loop [i 0]
+       (when (< i ttl)
+         (let [x (nth xs i)
+               pair (- amount x)
+               set-list (set (rest xs))]
+           (if (contains? set-list pair)
+             [x pair]
+             (recur (inc i)))))))))
+
+(defn first-part
+  "Find matching pair and multiply all"
+  [input]
+  (->> input find-match-pair (apply *)))
+
+(defn find-match-triplet
+  "Find matching triplet that sums up to the amount"
+  ([xs] (find-match-triplet xs 2020))
+  ([xs amount]
+   (if-let [ttl (count xs)] 
+     (loop [i 0]
+       (when (< i ttl)
+         (let [x (nth xs i)
+               pair (find-match-pair xs (- amount x))]
+           (if pair 
+             (conj pair x)
+             (recur (inc i)))))))))
+
+(defn second-part
+  "Find matching triplet and multiply all"
+  [xs]
+  (->> input find-match-triplet (apply *)))
+
+(comment
+  ; Answer - 482811
+  (first-part input)
+  ; Answer - 193171814
+  (second-part input)
+)
