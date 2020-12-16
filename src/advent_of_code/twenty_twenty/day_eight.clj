9c3ca98ad607d935b441a1da5d96f1429e165cb2
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2020-12-17 00:41:06 +1100

    Day 8 part 2 ✅

--- src/advent_of_code/twenty_twenty/day_eight.clj
+++ src/advent_of_code/twenty_twenty/day_eight.clj
@@ -1,18 +1,6 @@
 (ns advent-of-code.twenty-twenty.day-eight
-  (:require [clojure.string :refer [split-lines join split] :as st]
+  (:require [clojure.string :refer [split-lines join split includes?] :as st]
             [clojure.java.io :refer [resource]]))
-
-(def sample "nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6")
-
-(def parsed (split-lines sample))
 
 (def input (split-lines (slurp (resource "day_eight_input.txt"))))
 
@@ -35,7 +23,48 @@ acc +6")
       )
     ))
 
+(defn traverse-modified [pos acc visit transformed]
+  (cond
+    (contains? visit pos)
+    :infinite
+
+    (= (count input) (inc pos))
+    acc
+
+    :else
+    (let [[_ op v] (parse (get input pos))]
+      (cond
+        (= "acc" op)
+        (traverse-modified (inc pos) (+ acc (parseInt v)) (conj visit pos) transformed)
+
+        (= "jmp" op)
+        (if (contains? transformed pos)
+          (traverse-modified (inc pos) acc (conj visit pos) (conj transformed pos))
+          (traverse-modified (+ pos (parseInt v)) acc (conj visit pos) transformed))
+
+        :else
+        (if (contains? transformed pos)
+          (traverse-modified (+ pos (parseInt v)) acc (conj visit pos) (conj transformed pos))
+          (traverse-modified (inc pos) acc (conj visit pos) transformed))))))
+
+(defn traverse-fix
+  []
+  (let [action-map (apply merge
+                          (map (fn [x y]
+                                 (if (or (includes? y "jmp")
+                                         (includes? y "nop"))
+                                   {x y} {}))
+                               (range (count input))
+                               input))
+        action-keys (keys action-map)]
+    (first
+      (filter #(not= :infinite %)
+              (map #(traverse-modified 0 0 #{} #{%}) action-keys)))))
+
 (comment
   ; First part - 1867
   (traverse 0 0 #{})
+
+  ; Second part - ?
+ (traverse-fix) 
 )
